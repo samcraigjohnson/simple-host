07d28186f67474ec345182f5b2c84eaff7244edc
Sam Johnson <sam@devscoach.com>
2024-02-19 14:36:32 -0800

    Add signups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :newsletter_signups, only: :create
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
